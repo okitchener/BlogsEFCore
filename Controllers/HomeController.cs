c01a1a1554cbc70147b7d622a705cf3ba3c7ac11
Owen Kitchener <okitchener@my.wctc.edu>
2026-02-23 08:59:12 -0600

    [1.09] Sort Blogs by Name

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 public class HomeController(DataContext db) : Controller
 {
   // this controller depends on the DataContext
   private readonly DataContext _dataContext = db;
 
-  public IActionResult Index() => View(_dataContext.Blogs);
+  public IActionResult Index() => View(_dataContext.Blogs.OrderBy(b => b.Name));
 }
