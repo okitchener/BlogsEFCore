eb694fbc4c066437a240047d8390353eca235392
Owen Kitchener <okitchener@my.wctc.edu>
2026-03-02 09:27:25 -0600

    [2.02] Complete database Update (Add Blog)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,5 +8,59 @@ public class HomeController(DataContext db) : Controller
 
   public IActionResult Index() => View(_dataContext.Blogs.OrderBy(b => b.Name));
 
-public IActionResult AddBlog() => View(new Blog());
+  public IActionResult AddBlog() => View(new Blog());
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public IActionResult AddBlog(Blog model)
+  {
+    if (ModelState.IsValid)
+    {
+      if (_dataContext.Blogs.Any(b => b.Name == model.Name))
+      {
+        ModelState.AddModelError("", "Name must be unique");
+      }
+      else
+      {
+        _dataContext.AddBlog(model);
+        return RedirectToAction("Index");
+      }
+    }
+    return View();
+  }
+  public IActionResult DeleteBlog(int id)
+  {
+    _dataContext.DeleteBlog(_dataContext.Blogs.FirstOrDefault(b => b.BlogId == id));
+    return RedirectToAction("Index");
+  }
+  public IActionResult BlogDetail(int id) => View(new PostViewModel
+  {
+    blog = _dataContext.Blogs.FirstOrDefault(b => b.BlogId == id),
+    Posts = _dataContext.Posts.Where(p => p.BlogId == id)
+  });
+  public IActionResult AddPost(int id)
+  {
+    ViewBag.BlogId = id;
+    return View(new Post());
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public IActionResult AddPost(int id, Post post)
+  {
+    post.BlogId = id;
+    if (ModelState.IsValid)
+    {
+      _dataContext.AddPost(post);
+      return RedirectToAction("BlogDetail", new { id = id });
+    }
+    @ViewBag.BlogId = id;
+    return View();
+  }
+  public IActionResult DeletePost(int id)
+  {
+    Post post = _dataContext.Posts.FirstOrDefault(p => p.PostId == id);
+    int BlogId = post.BlogId;
+    _dataContext.DeletePost(post);
+    return RedirectToAction("BlogDetail", new { id = BlogId });
+  }
 }
--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Blog
 {
   public int BlogId { get; set; }
+   [Required]
   public string Name { get; set; }
 
   public ICollection<Post> Posts { get; set; }
