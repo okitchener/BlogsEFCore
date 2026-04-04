4e85791a0f53bbdedb60be4aa9c5fd22aa4348f2
Owen Kitchener <okitchener@my.wctc.edu>
2026-04-04 12:42:52 -0500

    [3.02] Identity model

---
+++ Models/AppIdentityDbContext.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : IdentityDbContext<AppUser>(options)
+{
+  
+}
